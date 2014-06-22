8c81cf0e5b0985d5ce776a520aa445ed6aa752dc
Steven Kusalo <smkusalo@gmail.com>
2014-06-22 11:27:07 -0700

    finished I3 tagging in blogger tutorial

---
+++ wk04/d1/blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,18 @@
+class TagsController < ApplicationController
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    Tag.find(params[:id]).destroy
+    flash.notice = "Tag successfully deleted!"
+
+    redirect_to tags_path
+
+  end
+end
